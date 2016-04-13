1d4dbcd5f9b31195ec0302eccfce84ae47202d04
Timothy Russell-Wagner <atonement100@gmail.com>
2016-04-13 19:22:07 -0400

    simple example of movie texture
    bunch of random binary files pushed here as well, rollback if any issues
    are caused

---
+++ Concussion/Assets/startMovieOnPlay.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class startMovieOnPlay : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        ((MovieTexture)GetComponent<Renderer>().material.mainTexture).Play();
+        GetComponent<AudioSource>().Play();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
