aad3e2827c3dff3410575bde9cccc832d6a81ddf
Timothy Russell-Wagner <atonement100@gmail.com>
2016-04-19 00:26:40 -0400

    raycast

--- Concussion/Assets/playerController.cs
+++ Concussion/Assets/playerController.cs
@@ -13,35 +13,15 @@ public class playerController : MonoBehaviour {
 	    if (Cardboard.SDK.Triggered)
         {
             //whatever trigger logic we want here
-            GetComponent<Transform>().parent.transform.position += new Vector3 (GetComponent<Transform>().transform.forward.x * Time.deltaTime * 10, 0.0f, GetComponent<Transform>().forward.z*Time.deltaTime*10);
+
+            //movement
+            //GetComponent<Transform>().parent.transform.position += new Vector3 (GetComponent<Transform>().transform.forward.x * Time.deltaTime * 10, 0.0f, GetComponent<Transform>().forward.z*Time.deltaTime*10);
+
+            //raycast for selection of options
+            if (Physics.Raycast(GetComponent<Transform>().parent.transform.position, GetComponent<Transform>().transform.forward, 1000.0f))
+            {
+                print("hit");
+            }
         }
 	}
 }
-/*
-float tiltAngle = Cardboard.SDK.HeadPose.Orientation.eulerAngles.z;
-		if (GetComponent<Rigidbody>().transform != null)
-		{
-			GetComponent<Rigidbody>().rotation = Quaternion.Euler(0.0f, 0.0f, tiltAngle);
-		}
-
-		if (tiltAngle > 180) { tiltAngle = -(360 - tiltAngle); }
-
-		tiltAngle = -tiltAngle/45;
-
-		float moveHorizontal = Input.GetAxis("Horizontal");
-//float moveVertical = Input.GetAxis ("Vertical");
-
-Vector3 movement = new Vector3(tiltAngle, 0.0f, 0.0f);
-
-		if (GetComponent<Rigidbody>().transform != null)
-		{
-			GetComponent<Rigidbody>().velocity = movement* speed;
-
-GetComponent<Rigidbody>().position = new Vector3
-    (
-        Mathf.Clamp(GetComponent<Rigidbody>().position.x, boundary.xMin, boundary.xMax),
-					0.0f,
-					Mathf.Clamp(GetComponent<Rigidbody>().position.z, boundary.zMin, boundary.zMax)
-				);
-		}
-        */
