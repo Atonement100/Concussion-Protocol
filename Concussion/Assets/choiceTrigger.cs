2eb13da4f7e36f2f792e71014492ce425ba94a29
Timothy Russell-Wagner <atonement100@gmail.com>
2016-04-19 23:50:08 -0400

    choice selection

---
+++ Concussion/Assets/choiceTrigger.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class choiceTrigger : MonoBehaviour {
+    public int choiceDegree, choiceVar, firstChoice;
+    public GameObject otherTrigger;
+
+	// Use this for initialization
+	void Start () {
+        print("loaded");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}
--- Concussion/Assets/playerController.cs
+++ Concussion/Assets/playerController.cs
@@ -18,10 +18,61 @@ public class playerController : MonoBehaviour {
             //GetComponent<Transform>().parent.transform.position += new Vector3 (GetComponent<Transform>().transform.forward.x * Time.deltaTime * 10, 0.0f, GetComponent<Transform>().forward.z*Time.deltaTime*10);
 
             //raycast for selection of options
-            if (Physics.Raycast(GetComponent<Transform>().parent.transform.position, GetComponent<Transform>().transform.forward, 1000.0f))
+            RaycastHit hit;
+            if (Physics.Raycast(GetComponent<Transform>().parent.transform.position, GetComponent<Transform>().transform.forward, out hit))
             {
+                GameObject wasHit = hit.transform.gameObject;
                 print("hit");
+                if(wasHit.tag == "decisionTrigger") {
+                    choiceTrigger picked = wasHit.GetComponent("choiceTrigger") as choiceTrigger;
+                    choiceTrigger unpicked = picked.otherTrigger.GetComponent("choiceTrigger") as choiceTrigger;
+                    if (picked != null)
+                    {
+                        if (picked.choiceDegree == 0)
+                        {
+                            picked.choiceDegree++;
+                            unpicked.choiceDegree++;
+                            picked.firstChoice = picked.choiceVar;
+                            unpicked.firstChoice = picked.choiceVar;
+
+                            if(picked.firstChoice == 0)
+                            {
+                                print("first choice was A");
+                            }
+                            else
+                            {
+                                print("first choice was B");
+                            }
+                        }
+                        else
+                        {
+                            if (picked.firstChoice == 0)
+                            {
+                                if(picked.choiceVar == 0)
+                                {
+                                    print("first choice was A, second choice was A");
+                                }
+                                else
+                                {
+                                    print("first choice was A, second choice was B");
+                                }
+                            }
+                            else
+                            {
+                                if (picked.choiceVar == 1)
+                                {
+                                    print("first choice was B, second choice was B");
+                                }
+                                else
+                                {
+                                    print("first choice was B, second choice was A");
+                                }
+                            }
+                        }
+                    }
+                }
             }
+            else { print("miss"); }
         }
 	}
 }
