468d30a09c378fa928e8be970262928b576b1606
Timothy Russell-Wagner <atonement100@gmail.com>
2016-04-23 18:46:53 -0400

    date visualization in hud set up
    may not be able to go during black screen, could potentially write over
    the black fade screen with guitext however, if we really would like it
    to be during the transition instead of at the start of each scene

---
+++ Concussion/Assets/updateText.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class updateText : MonoBehaviour {
+    public Text text;
+
+    public void changeText(string newText)
+    {
+        StartCoroutine(changeTextHandler(newText));    
+    }
+
+    IEnumerator changeTextHandler(string newText)
+    {
+        text.text = newText;
+        yield return new WaitForSeconds(2.0f);
+        text.text = " ";
+        yield break;
+    }
+}
