b3b8426bc404bfc2650892d88ae2c372b024aa7f
Timothy Russell-Wagner <atonement100@gmail.com>
2016-04-26 16:46:10 -0400

    hacky npc movement
    example usage in football scene. Attached movementController script to
    model you want to move. Place any number of objects (i.e. small cubes w.
    no colliders) as waypoints. On the attached script, click on
    destination, put in size = number of objects you placed. Drag in
    sequential order the objects into the script. This works for any number
    of models moving and each can have their own waypoints

---
+++ Concussion/Assets/destinationHandler.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class destinationHandler : MonoBehaviour {
+    [Tooltip("Set this to the destination that this route should lead to next.")]
+    public GameObject nextDestination; 
+	
+
+
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}
---
+++ Concussion/Assets/movementController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movementController : MonoBehaviour {
+    public float speed = 50.0f;
+    public bool isMoving = true;
+    public GameObject[] destination;
+
+    private float before, after;
+    private int i = 0;
+
+    void Start()
+    {
+        for (int i = 0; i < destination.Length; i++)
+        {
+            destination[i] = new Vector3(destination[i].x, transform.position.y, destination[i].z);
+        }
+    }
+
+	void Update () {
+	    if (isMoving)
+        {
+            before = transform.position.magnitude;
+
+            float dist = speed * Time.deltaTime;
+            transform.LookAt(destination[i].transform);
+            transform.position = Vector3.MoveTowards(transform.position, destination[i].transform.position, dist);
+
+            after = transform.position.magnitude;
+            print(after - before);
+
+
+            if((after - before) == 0)
+            {
+                if ((i + 1) < destination.Length) { i++; }
+                else { isMoving = false; }
+            }
+
+
+        }
+	}
+}
