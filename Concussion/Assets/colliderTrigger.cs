eeed8f647f03b828270b4b6df773f0a9f2488554
Marie Urmano <marieeurmano@gmail.com>
2016-04-26 23:18:38 -0400

    Added sound when user gets hit by the football player

---
+++ Concussion/Assets/colliderTrigger.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class colliderTrigger : MonoBehaviour {
+
+	private AudioSource audioSource;
+	public AudioClip scream;
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+		audioSource = GetComponent<AudioSource>();
+		audioSource.clip = scream;
+		//audio.Pause ();
+
+	}
+
+	void OnTriggerEnter(Collider other){
+
+
+		if(other.gameObject.tag == "footballImpact"){
+			//print ("hi");
+			audioSource.Play ();
+		}
+	}
+}
